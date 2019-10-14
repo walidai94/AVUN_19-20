4335656e87ce833cab7ca48ee7cc71b5fde51bd8
Sam Szo <samuel.szoniecky@univ-paris8.fr>
2019-10-14 12:55:40 +0200

    trombinoscope

---
+++ samszo/diigo.php
@@ -0,0 +1,10 @@
+<div class="diigo-tags">
+    <div class="diigo-banner sidebar-title" style="font: bold 12px arial;margin-bottom:5px;">
+        <a href="https://www.diigo.com">
+            <img src="https://www.diigo.com/images/ii_blue.gif" width="16" height="16" alt="diigo" /></a>
+        <a href="https://www.diigo.com/cloud/<?php echo $_GET['user'];?>">Tags Diigo</a>
+    </div>
+    <script type="text/javascript" 
+    src="https://www.diigo.com/tools/tagrolls_script/<?php echo $_GET['user'];?>?icon;size=11-23;color=87ceeb-0000ff;title=Diigo%20Tags;name;showadd;v=3">
+    </script>
+</div>
